daabbeb6bb7fcc4d2e81fdb65b56c7374813f6af
sl243 <slmiucse@gmail.com>
2022-09-02 22:48:47 +0600

    daynamic all news category show in the site

--- js/news.js
+++ js/news.js
@@ -2,7 +2,23 @@ const loadNewsCategory = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.news_category);
+    displayNewsCategory(data.data.news_category);
+}
+
+const displayNewsCategory = newsCategory => {
+    // console.log(newsCategory);
+    const navbarContainer = document.getElementById('navbar-container');
+    newsCategory.forEach(news => {
+        console.log(news);
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML = `
+        <li class="nav-item">
+        <button type="button" class="btn btn-light">${news.category_name}</button>           
+        </li>
+        `;
+        navbarContainer.appendChild(li);
+    })
 }
 
 loadNewsCategory();
