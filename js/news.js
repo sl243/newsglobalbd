3c9e6cc50744c6b6bfd399d9e1385cf570075a78
sl243 <slmiucse@gmail.com>
2022-09-02 22:26:06 +0600

    all news category added

--- js/news.js
+++ js/news.js
@@ -1,1 +1,8 @@
-console.log('javaScript file added')
+const loadNewsCategory = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.news_category);
+}
+
+loadNewsCategory();
